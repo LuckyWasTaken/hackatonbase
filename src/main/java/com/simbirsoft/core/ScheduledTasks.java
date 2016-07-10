a1509824b836370cb8e50535a758f9d165a142ed
Alex <Alex@ALEX-PC.simbirsoft>
2016-07-10 16:33:56 +0400

    Post method was corrected

--- src/main/java/com/simbirsoft/core/ScheduledTasks.java
+++ src/main/java/com/simbirsoft/core/ScheduledTasks.java
@@ -44,36 +44,35 @@ public class ScheduledTasks {
             smart.switchDevice("FRIDGE", true);
         else
             smart.switchDevice("FRIDGE", false);
-
-        if (detectflood()) {
-            smart.switchDevice("MICROWAVE", false);
-        } else {
-            smart.switchDevice("MICROWAVE", true);
-        }
-
-        actionOnFire();
     }
-     
-    private boolean actionOnFire(){
+    @Scheduled(fixedRate = 120000) 
+    public void actionOnFire(){
 
         boolean is_fire = (Double.parseDouble(smart.getStatusSensor("ROOM_TEMPERATURE"))>100)
                 &&(Double.parseDouble(smart.getStatusSensor("SMOKE"))>0);
 
         if (is_fire) {
             smart.callExternalService("FIRE_DEPARTMENT");
-
             smart.switchDevice("VENTILATION", true);
-
             smart.switchDevice("MICROWAVE", false);
             smart.switchDevice("TV",false);
             smart.switchDevice("HOOVER", false);
         }
 
-        return is_fire;
+        
     }
 
-    private boolean  detectflood(){
-        return (Double.parseDouble(smart.getStatusSensor("WATER_LEAK"))>1);
+    @Scheduled(fixedRate = 120000) 
+    public void actionOnFlood(){
+        boolean is_flood = (Double.parseDouble(smart.getStatusSensor("WATER_LEAK"))>0);
+        if (is_flood)
+        {
+            smart.callExternalService("PLUMBER");
+            smart.switchDevice("MICROWAVE", false);
+            smart.switchDevice("TV",false);
+            smart.switchDevice("HOOVER", false);
+        }
+        
 
     }
 }
