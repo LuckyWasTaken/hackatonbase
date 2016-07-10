800fe7b28e6447a544695734bb926e478d72bb58
Valery Malyshev <vmalyshev@cs-cart.com>
2016-07-10 16:06:09 +0400

    run

--- run.sh
+++ run.sh
@@ -7,26 +7,30 @@ I=$(( I + 1));
 echo "$I"
 
 curl "http://localhost:8080/tv/on"
-curl "http://localhost:8080/vent/off"
-curl "http://localhost:8080/hv/off"
+curl "http://localhost:8080/vent/on"
+curl "http://localhost:8080/hv/on"
+curl "http://localhost:8080/microwave/on"
+curl "http://localhost:8080/fridge/on"
 
-sleep 1
+sleep 2
 
 curl "http://localhost:8080/tv/off"
 curl "http://localhost:8080/vent/on"
 curl "http://localhost:8080/hv/on"
 
-sleep 1
+sleep 2
 
 curl "http://localhost:8080/tv/off"
 curl "http://localhost:8080/vent/off"
+curl "http://localhost:8080/microwave/off"
 
-sleep 1
+sleep 2
 
 curl "http://localhost:8080/tv/on"
 curl "http://localhost:8080/vent/on"
 curl "http://localhost:8080/hv/off"
+curl "http://localhost:8080/fridge/off"
 
-sleep 1
+sleep 2
 
 done 
