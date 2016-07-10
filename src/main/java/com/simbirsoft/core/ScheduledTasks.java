e6abe40ec9fc63ba87940f942c3dd89224d8360e
Alex <Alex@ALEX-PC.simbirsoft>
2016-07-10 15:57:08 +0400

    Post method was corrected

--- src/main/java/com/simbirsoft/core/ScheduledTasks.java
+++ src/main/java/com/simbirsoft/core/ScheduledTasks.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledTasks {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
     @Autowired
     private SmarthomeService smart;
     
